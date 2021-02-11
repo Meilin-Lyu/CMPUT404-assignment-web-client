d478c7a1d3a785afc98767b35514c9d4cfffb4ad
Meilin-Lyu <mlyu@ualberta.ca>
2021-02-11 00:48:24 -0700

    done

--- README.md
+++ README.md
@@ -18,3 +18,27 @@ examples thus some of the code is Copyright © 2001-2013 Python
 Software Foundation; All Rights Reserved under the PSF license (GPL
 compatible) http://docs.python.org/2/library/socketserver.html
 
+Citation:
+========================
+stolen from Adam Smith https://stackoverflow.com/users/3058609/adam-smith
+From Stackoverflow
+https://stackoverflow.com/questions/40557606/how-to-url-encode-in-python-3
+"result = urlencode(payload, quote_via=quote_plus)" I learnt how to use urlencode
+and used in line 44, 148, 156 in httpclient.py
+
+stolen from stwhite https://stackoverflow.com/users/415763/stwhite
+From Stackoverflow
+https://stackoverflow.com/questions/10115126/python-requests-close-http-connection
+"r = requests.post(url=url, data=body, headers={'Connection':'close'})" I learnt how to use "connection:close"
+and used in line 126, 164 in httpclient.py
+
+refer to https://docs.python.org/3/library/urllib.parse.html
+"urllib.parse.urlparse(urlstring, scheme='', allow_fragments=True)"
+ used in line 44, 60, 71 in httpclient.py
+ Example of http GET and POST Request Message is from 
+ https://www.tutorialspoint.com/http/http_requests.htm#:~:text=The%20GET%20method%20is%20used,other%20effect%20on%20the%20data. 
+
+ Collaboration:
+ ========================
+ I consult with Shuwei Wang(ccid:shuwei4) about using "connection: close" in payload to reduce running time, fields in payload and if we need to handle args in GET request method.
+
--- httpclient.py
+++ httpclient.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 # coding: utf-8
-# Copyright 2016 Abram Hindle, https://github.com/tywtyw2002, and https://github.com/treedust
+# Copyright 2016 Abram Hindle, https://github.com/tywtyw2002, and https://github.com/treedust, 2021 Meilin Lyu
 # 
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -18,11 +18,30 @@
 # Write your own HTTP GET and POST
 # The point is to understand what you have to send and get experience with it
 
+# Citation:
+# stolen from Adam Smith https://stackoverflow.com/users/3058609/adam-smith
+# From Stackoverflow
+# https://stackoverflow.com/questions/40557606/how-to-url-encode-in-python-3
+# "result = urlencode(payload, quote_via=quote_plus)" I learnt how to use urlencode
+# and used in line 44, 148, 156
+
+# stolen from stwhite https://stackoverflow.com/users/415763/stwhite
+# From Stackoverflow
+# https://stackoverflow.com/questions/10115126/python-requests-close-http-connection
+# "r = requests.post(url=url, data=body, headers={'Connection':'close'})" I learnt how to use "connection:close"
+# and used in line 126, 164
+
+# refer to https://docs.python.org/3/library/urllib.parse.html
+# "urllib.parse.urlparse(urlstring, scheme='', allow_fragments=True)"
+#  used in line 44, 60, 71
+#  Example of http GET and POST Request Message is from 
+#  https://www.tutorialspoint.com/http/http_requests.htm#:~:text=The%20GET%20method%20is%20used,other%20effect%20on%20the%20data. 
+
 import sys
 import socket
 import re
 # you may use urllib to encode data appropriately
-import urllib.parse
+from urllib.parse import urlparse, urlencode
 
 def help():
     print("httpclient.py [GET/POST] [URL]\n")
@@ -33,22 +52,48 @@ class HTTPResponse(object):
         self.body = body
 
 class HTTPClient(object):
-    #def get_host_port(self,url):
+    def get_host_port(self,url):
+        ''' This function is used to get host name and port
+        from the url '''
+        # default port
+        port = 80
+        parse_url = urlparse(url)
+        host = parse_url.hostname
+        # if url contains the port, then get the port
+        if parse_url.port:
+            port = parse_url.port
+        return host,port
+        
+    def get_path(self,url):
+        '''This function is used to get the path from url'''
+        # default path
+        path = "/"
+        parse_url = urlparse(url)
+        # if url contains the path, then get the path
+        if parse_url.path:
+            path = parse_url.path
+        return path
 
     def connect(self, host, port):
         self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self.socket.connect((host, port))
-        return None
 
     def get_code(self, data):
-        return None
+        # The code is obtained in the first line of header
+        header = self.get_headers(data)
+        code = int(header.split()[1])
+        return code
 
     def get_headers(self,data):
-        return None
-
+        # get the header from data
+        header = data.split('\r\n\r\n')[0]
+        return header
+        
     def get_body(self, data):
-        return None
-    
+        # get body from data
+        body = data.split('\r\n\r\n')[1]
+        return body
+
     def sendall(self, data):
         self.socket.sendall(data.encode('utf-8'))
         
@@ -66,15 +111,69 @@ class HTTPClient(object):
             else:
                 done = not part
         return buffer.decode('utf-8')
-
+   
     def GET(self, url, args=None):
-        code = 500
-        body = ""
+        # get the host and port from url
+        host,port = self.get_host_port(url)
+        # get the path from url
+        path = self.get_path(url)
+        # connect to socket
+        self.connect(host,port)
+        # format the payload
+        payload = "GET " +path+ " HTTP/1.1\r\n"
+        payload += "Host: " +host+ "\r\n"
+        # to reduce running time
+        payload += "Connection: close"+"\r\n\r\n"
+        self.sendall(payload)
+        # get the data
+        full_data = self.recvall(self.socket)
+        self.close()
+        # get code from data received
+        code = self.get_code(full_data)
+        # get body from data received
+        body = self.get_body(full_data)
+        # print out the result for user
+        print("url: ",url)
+        print("code: ",code)
+        print("port: ",port)
+        print("body: ",body)
         return HTTPResponse(code, body)
 
     def POST(self, url, args=None):
-        code = 500
-        body = ""
+        # get the host and port from url
+        host,port = self.get_host_port(url)
+        # get the path from url
+        path = self.get_path(url)
+        # Initialize the content,content_length,content_type
+        content = urlencode("")
+        content_length = 0
+        content_type = "application/x-www-form-urlencoded"
+        # connent to socket
+        self.connect(host,port)
+        # if the content is not empty
+        # get the content and content length
+        if args:
+            content = urlencode(args)
+            content_length = len(content)
+        # format the payload
+        payload = "POST " +path+ " HTTP/1.1\r\n"
+        payload += "Host: " +host+ "\r\n"
+        payload += "Content-Type:" +content_type+ "\r\n"
+        payload += "Content-Length: "+ str(content_length)+ "\r\n"
+        # to reduce running time
+        payload += "Connection: close"+"\r\n\r\n"
+        # add the content to payload
+        payload += content+ "\r\n\r\n"  
+        self.sendall(payload)
+        # get the data
+        full_data = self.recvall(self.socket)
+        self.close()
+        code = self.get_code(full_data)
+        body = self.get_body(full_data)
+        print("url: ",url)
+        print("code: ",code)
+        print("port: ",port)
+        print("body: ",body)
         return HTTPResponse(code, body)
 
     def command(self, url, command="GET", args=None):
